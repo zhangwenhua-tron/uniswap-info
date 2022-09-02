3119dfa4e611714316eb07c9725dc2d55fb966a5
Wenhua Zhang <wayne.zhang@tron.network>
2022-09-02 21:09:37 +0800

    feat: update block client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -39,7 +39,7 @@ export const stakingClient = new ApolloClient({
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     // uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
-    uri: 'http://54.214.249.115:8000/subgraphs/name/zhangwenhua-tron/tron-blocks',
+    uri: 'http://54.214.249.115:8000/subgraphs/name/zhangwenhua-tron/sunswap-blocks',
   }),
   cache: new InMemoryCache(),
 })
