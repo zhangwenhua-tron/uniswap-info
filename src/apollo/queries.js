78c5743799510333fb538927943ebe7a7d5a875d
Wenhua Zhang <wayne.zhang@tron.network>
2022-09-04 23:42:35 +0800

    feat: update SUBGRAPH_HEALTH

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "uniswap/uniswap-v2") {
+    indexingStatusForCurrentVersion(subgraphName: "zhangwenhua-tron/sunswap") {
       synced
       health
       chains {
