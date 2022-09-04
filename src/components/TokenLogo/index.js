97e9c3dc31ad5a1337759c89b669fdadb3a88300
Wenhua Zhang <wayne.zhang@tron.network>
2022-09-05 00:05:58 +0800

    feat: update token logo

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -71,9 +71,7 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
-    address
-  )}/logo.png`
+  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/$0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png`
 
   return (
     <Inline>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,9 +8,7 @@ import copy from 'copy-to-clipboard'
 export function useColor(tokenAddress, token) {
   const [color, setColor] = useState('#2172E5')
   if (tokenAddress) {
-    const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
-      tokenAddress
-    )}/logo.png`
+    const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png`
     if (path) {
       Vibrant.from(path).getPalette((err, palette) => {
         if (palette && palette.Vibrant) {
